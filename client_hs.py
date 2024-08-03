ace0a1fde2eea1731577529ea0b5020ffa7f713f
Shumpei Chochi <shumpei116@gmail.com>
2024-08-03 09:44:28 +0900

    feat: Add support for high and low scan rates in Client class

--- client_hs.py
+++ client_hs.py
@@ -1,9 +1,4 @@
-from base import InetClient
-
-
-class ClientHS:
-    def __init__(self, host: str, port: int):
-        self.client = InetClient(host, port)
+from base.client import Client
 
 
 if __name__ == "__main__":
@@ -11,11 +6,25 @@ if __name__ == "__main__":
         try:
             host = input("Enter host: ")
             port = int(input("Enter port[8080]: "))
+            hs_save_dir = input("Enter save directory: ")
+            high_scan_rate = int(input("Enter high scan rate: "))
+            low_scan_rate = int(input("Enter low scan rate: "))
+            high_scan_second = int(input("Enter high scan second: "))
+            low_scan_second = int(input("Enter low scan second: "))
+
             if port == "":
                 port = 8080
 
             dataset_dir = input("Enter dataset directory: ")
-            client = ClientHS(host, port)
+            client = Client(
+                host,
+                port,
+                hs_save_dir,
+                high_scan_rate,
+                low_scan_rate,
+                high_scan_second,
+                low_scan_second,
+            )
             client.client.send()
         except Exception as e:
             print(e)
